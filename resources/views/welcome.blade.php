4b10f4f945fc6091d35e87a1c78fe4f167b91cf2
Mauritz Erick <mauritz.erick@gmail.com>
2021-07-04 02:21:51 +1000

    links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
 @section('content')
+    
+    <div class="flex bg-gray-100 border-b border-gray-300">
+        <div class="container mx-auto">
+            <router-link to='/'>Home</router-link>
+            <router-link to='/'>About </router-link>
+        </div>
+    </div>
     <router-view></router-view>
 @endsection
